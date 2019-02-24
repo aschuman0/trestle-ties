4dfe4e2460d6e9d8d156652edec8a3a082ec9a86
Aaron Schuman <aaron@aschuman.net>
2019-02-24 14:22:14 -0500

    package restucture

--- run.py
+++ run.py
@@ -7,35 +7,43 @@ app.url_map.strict_slashes = False
 def index():
     return render_template('index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 @app.route('/submissions')
 def submissions():
     return render_template('submissions.html')
 
+
 @app.route('/issues')
 def issue_index():
     return render_template('issues/issue_index.html')
 
+
 @app.route('/issues/one')
 def issue_one():
-    return render_template('issues/one.html')
+    return render_template('issues/one/index.html')
+
 
 @app.route('/issues/one/contributors')
 def issue_one_contributors():
-    return render_template('issues/one_contributors.html')
+    return render_template('issues/one/contributors.html')
+
 
 @app.errorhandler(404)
 def not_found(error):
     error = 'Page Not Found'
     return render_template('error.html', error=error)
 
+
 @app.errorhandler(500)
 def server_error(error):
     error = 'Server Error'
     return render_template('error.html', error=error)
+
 
 if __name__ == '__main__':
     app.run()
